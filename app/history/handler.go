7afcccb2f72522dc31049a69be1237e7eb42f7a7
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-07-12 20:18:03 +0700

    fix error

--- app/history/handler.go
+++ app/history/handler.go
@@ -3,6 +3,8 @@ package history
 import (
 	"context"
 	"net/http"
+	"strconv"
+
 	// "strconv"
 	"time"
 
@@ -32,13 +34,29 @@ func (h *Handler) GenerateMessage(c *gin.Context) {
 
 	var req NewHistoryRequest
 	
-	// Bind query parameters to the struct
-	if err := c.ShouldBindQuery(&req); err != nil {
-		c.JSON(http.StatusBadRequest, map[string]string{
-			"error": err.Error(),
-		})
+	req.UserID = c.Query("UserID")
+	agentID, err := strconv.Atoi(c.Query("AgentID"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid AgentID"})
 		return
 	}
+	req.AgentID = agentID
+
+	frameworkID, err := strconv.Atoi(c.Query("FrameworkID"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid FrameworkID"})
+		return
+	}
+	req.FrameworkID = frameworkID
+
+	req.Prompt = c.Query("Prompt")
+
+	styleMessageID, err := strconv.Atoi(c.Query("StyleMessageID"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid StyleMessageID"})
+		return
+	}
+	req.StyleMessageID = styleMessageID
 
 	language := c.GetString("language")
 	if language == "" {
